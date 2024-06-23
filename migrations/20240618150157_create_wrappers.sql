8d7bf0c3807407ee1fa020372eafe01db167cd46
khostya <khostya.konstantin@gmail.com>
2024-06-24 00:53:38 +0500

    [GOHW-4]: исправил отступ

--- migrations/20240618150157_create_wrappers.sql
+++ migrations/20240618150157_create_wrappers.sql
@@ -4,7 +4,7 @@ create table if not exists ozon.wrappers
 (
     order_id       text primary key references ozon.orders (id),
     type           ozon.wrapper not null,
-    price_in_rub   numeric       not null,
+    price_in_rub   numeric      not null,
     capacity_in_kg float4       not null
 );
 -- +goose StatementEnd
